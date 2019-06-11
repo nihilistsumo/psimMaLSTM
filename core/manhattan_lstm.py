886b2b59be4775742a257f06d7e4a16135711b53
Sumanta  Kashyapi <sk1105@wildcats.unh.edu>
2019-06-10 23:53:28 -0400

    lstm layer size fix add argument dist 2

--- core/manhattan_lstm.py
+++ core/manhattan_lstm.py
@@ -77,7 +77,7 @@ def malstm(Xtrain, ytrain, Xval, yval, Xtest, ytest, seq_len, vec_len, lstm_laye
     concats = concatenate([l1_out, l2_out], axis=-1)
     #
     # dist_output = Lambda(exponent_neg_cosine_distance, output_shape=(1,), name='distance')(concats)
-    dist_output = Lambda(exponent_neg_manhattan_distance, output_shape=(1,), name='distance')(concats, lstm_layer_size)
+    dist_output = Lambda(exponent_neg_manhattan_distance, output_shape=(1,), name='distance')((concats, lstm_layer_size))
     main_output = Dense(1, activation='relu')(dist_output)
     # else:
     #     main_output = Lambda(exponent_neg_manhattan_distance, output_shape=(1,))(concats)
