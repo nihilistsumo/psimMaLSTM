9e5e6a599f0675be4b805240b5fd874121505cb8
Sumanta  Kashyapi <sk1105@wildcats.unh.edu>
2019-07-26 12:10:25 -0400

    dense siam parapair dist add

--- core/dense_siam_parapair_dist.py
+++ core/dense_siam_parapair_dist.py
@@ -18,3 +18,34 @@ def get_Xtest(parapairs, embeddings):
 def get_parapair_dist(model, parapairs, embeddings, vec_len):
     Xtest = get_Xtest(parapairs, embeddings)
     yhat = model.predict([Xtest[:, :vec_len], Xtest[:, vec_len:]], verbose=0)
+    parapair_dist = dict()
+    for i in range(len(parapairs)):
+        parapair_dist[parapairs[i]] = float(yhat[i][0])
+    return parapair_dist
+
+def main():
+    parser = argparse.ArgumentParser(description="Produce parapair distance dict using pretrained Dense-Siamese model")
+    parser.add_argument("-m", "--model_file", required=True, help="Path to model file to load")
+    parser.add_argument("-pp", "--parapair", required=True, help="Path to test parapair file")
+    parser.add_argument("-em", "--embedding", required=True, help="Path to test embedding file")
+    parser.add_argument("-v", "--vec", required=True, type=int, help="Length of each paragraph vector")
+    parser.add_argument("-o", "--out", required=True, help="Path to parapair score output file")
+    args = vars(parser.parse_args())
+    model_file = args["model_file"]
+    parapair_file = args["parapair"]
+    emb_file = args["embedding"]
+    vec_len = args["vec"]
+    outfile = args["out"]
+
+    with open(parapair_file, 'r') as pp:
+        parapair_dict = json.load(pp)
+    parapair_list = []
+    for page in parapair_dict.keys():
+        parapair_list.extend(parapair_dict[page]["parapairs"])
+    emb = np.load(emb_file, allow_pickle=True)
+    model = load_model(model_file, custom_objects={'precision': dense_siamese.precision, 'recall': dense_siamese.recall,
+                                                   'fmeasure': dense_siamese.fmeasure,
+                                                   'fbeta_score': dense_siamese.fbeta_score})
+    parapair_dist = get_parapair_dist(model, parapair_list, emb, vec_len)
+    with open(outfile, 'w') as out:
+        json.dump(parapair_dist, out)
